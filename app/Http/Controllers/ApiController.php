76c41528ad250e764fee259be83db488e793e5f5
Nebojsa <nebojsa.urosevic@labs.devana.rs>
2015-10-27 16:17:59 +0100

    First commit for ApiController

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,9 +3,37 @@
 namespace StudentInfo\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
-abstract class ApiController extends BaseController
+class ApiController extends BaseController
 {
     use DispatchesJobs;
+
+    public function sendError($statusCode, $errorCode)
+    {
+        return new Response([
+            'error' => [
+                'errorCode' => $errorCode,
+                'message'   => config("errorMessages.{$errorCode}"),
+            ],
+        ], $statusCode);
+    }
+
+    public function sendSuccess($data)
+    {
+        return new Response([
+            'success' => [
+                'data'    => $data,
+                'message' => config("successMessages.{$data}"),
+            ],
+        ], 200);
+    }
+
+    public function sendForbidden($errorCode)
+    {
+        return $this->sendError(403, $errorCode);
+    }
+
+
 }
