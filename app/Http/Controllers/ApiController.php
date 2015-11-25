ae8e0fa63373e44120fb3aef889ed0860fc4302d
Nikola Ninkovic <nikola.ninkovic@labs.devana.rs>
2015-11-25 15:43:10 +0100

    Added serialization files

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,10 +5,29 @@ namespace StudentInfo\Http\Controllers;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
+use JMS\Serializer\SerializerBuilder;
 
 class ApiController extends BaseController
 {
     use DispatchesJobs;
+
+    public function returnSuccess(array $data = [])
+    {
+        $serializer = SerializerBuilder::create()
+            ->addMetadataDir(base_path() . '/serializations/')
+            ->build();
+        $jsonData   = $serializer->serialize($data, 'json');
+        return new Response([
+            'success' => [
+                'data' => $jsonData,
+            ],
+        ], 200);
+    }
+
+    public function returnForbidden($errorCode)
+    {
+        return $this->returnError(403, $errorCode);
+    }
 
     public function returnError($statusCode, $errorCode)
     {
@@ -18,20 +37,6 @@ class ApiController extends BaseController
                 'message'   => config("errorMessages.{$errorCode}"),
             ],
         ], $statusCode);
-    }
-
-    public function returnSuccess(array $data = [])
-    {
-        return new Response([
-            'success' => [
-                'data' => $data
-            ],
-        ], 200);
-    }
-
-    public function returnForbidden($errorCode)
-    {
-        return $this->returnError(403, $errorCode);
     }
 
 
