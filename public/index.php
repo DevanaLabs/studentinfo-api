c4edbb9a3a0aef1c45bb7e341b389faf82694741
Nebojsa <nebojsa.urosevic@labs.devana.rs>
2016-01-18 15:10:51 +0100

    session fix

--- public/index.php
+++ public/index.php
@@ -2,8 +2,7 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, X-Requested-With');
-header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 /**
  * Laravel - A PHP Framework For Web Artisans
