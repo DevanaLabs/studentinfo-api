b138ba0b6e4b82043dfb9f715b5243b6af666862
Nebojsa <nebojsa.urosevic@labs.devana.rs>
2016-02-05 16:43:23 +0100

    Guzzle Http in composer

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
