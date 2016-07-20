3ab10ded25a518cf81fca9503b134633d251c5f3
Nebojsa <nebojsa.urosevic@labs.devana.rs>
2016-07-20 13:25:26 +0200

    Typo fix

--- app/Jobs/SendInactiveBoardEmail.php
+++ app/Jobs/SendInactiveBoardEmail.php
@@ -40,7 +40,7 @@ class SendInactiveBoardEmail extends Job implements SelfHandling, ShouldQueue
         $email = $this->email;
 
         foreach ($this->inactiveBoards as $inactiveBoard) {
-            $mailer->send('emails.warning_inactive_board_mail_template.blade', [
+            $mailer->send('emails.warning_inactive_board_mail_template', [
                 'board' => $inactiveBoard->getSender(),
             ], function (Message $message) use ($email) {
                 $message->from('noreply@studentinfo.rs', 'StudentInfo');
